6a3736ae91034b8cb72a522ddaf1e448fc673a33
syunatsu <myf.tyys@gmail.com>
2022-05-04 22:14:20 +0900

    rubocop実施

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.includes(:user).order(created_at: "DESC")
+    @articles = Article.includes(:user).order(created_at: 'DESC')
   end
 end
