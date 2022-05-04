f12eb7d46c798508a5134436d8b791da2a8e5102
syunatsu <myf.tyys@gmail.com>
2022-05-04 16:45:33 +0900

    rubocop実施

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
-  
+
   describe 'ユーザー新規登録' do
     context '登録できるとき' do
       it 'nickname, email, password, password_confirmationが正しく入力されていれば登録できる' do
@@ -20,7 +20,7 @@ RSpec.describe User, type: :model do
       it 'nicknameが16文字以上だと登録できない' do
         @user.nickname = 'aaaaaaaa11111111'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 15 characters)")
+        expect(@user.errors.full_messages).to include('Nickname is too long (maximum is 15 characters)')
       end
       it 'emailが空では登録できない' do
         @user.email = ''
@@ -30,7 +30,7 @@ RSpec.describe User, type: :model do
       it 'emailに@が入っていないと登録できない' do
         @user.email = 'test.test'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Email is invalid")
+        expect(@user.errors.full_messages).to include('Email is invalid')
       end
       it 'passwordが空では登録できない' do
         @user.password = ''
@@ -40,12 +40,12 @@ RSpec.describe User, type: :model do
       it 'passwordが5文字以下では登録できない' do
         @user.password = 'aaa11'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
       it 'passwordが半角英数字以外では登録できない' do
         @user.password = 'パスワードパスワード'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is invalid")
+        expect(@user.errors.full_messages).to include('Password is invalid')
       end
       it 'passwordとpassword_confirmationが一致しないと登録できない' do
         @user.password_confirmation = 'password'
