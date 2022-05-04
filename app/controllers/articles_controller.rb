d3e1bb90e6463620bfcabcc5641bd7706a7a31e3
syunatsu <myf.tyys@gmail.com>
2022-05-04 22:13:37 +0900

    トップページの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.includes(:user).order(created_at: "DESC")
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   validates :name, presence: true
-  validates :url, format { with: /\A[a-z0-9]+\z/i}
+  validates :url, format: { with: /\A[a-z0-9]+\z/i }
   validates :category_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 8 }
 
   belongs_to :user
