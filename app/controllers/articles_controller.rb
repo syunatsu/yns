9ffa1f34820605982ae08077b415acd7023e89e8
syunatsu <myf.tyys@gmail.com>
2022-05-05 10:02:10 +0900

    new, createアクションの実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,22 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.includes(:user).order(created_at: 'DESC')
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def article_params
+    params.require(:artcle).permit(:name, :address, :url, :category_id).merge(user_id: current_user.id)
+  end
 end
