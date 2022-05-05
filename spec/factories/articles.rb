20c4204d7b1e60884eb8995c118f2f4811838bb9
syunatsu <myf.tyys@gmail.com>
2022-05-05 20:14:21 +0900

    モデル単体テストの実装

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,4 +1,13 @@
 FactoryBot.define do
   factory :article do
+    name        {Faker::Name.name}
+    address     {Faker::Address.city}
+    url         {Faker::Internet.url}
+    category_id {Faker::Number.within(range:1..9)}   
+    association :user
+
+    after(:build) do |article|
+      article.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe '新規投稿' do
+    context '投稿できるとき' do
+      it 'name, address, url, category_id, imageが正しく入力されていれば投稿できる' do
+        expect(@article).to be_valid
+      end
+      it 'addressが入力されていなくても投稿できる' do
+        expect(@article).to be_valid
+      end
+      it 'urlが入力されていなくても投稿できる' do
+        expect(@article).to be_valid
+      end
+    end
+    context '投稿できないとき' do
+      it 'nameが空だと投稿できない' do
+        @article.name = ''
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'category_idが0だと投稿できない' do
+        @article.category_id = 0
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'imageを添付していないと投稿できない' do
+        @article.image = nil
+        @article.valid?
+        expect(@article.errors.full_messages).to include("Image を1枚添付してください")
+      end
+      it 'userと紐づいていないと投稿できない' do
+        @article.user = nil
+        @article.valid?
+        expect(@article.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
