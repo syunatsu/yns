38ca25e09d86345a4588a6ed0616a19c955708ad
syunatsu <myf.tyys@gmail.com>
2022-05-05 12:51:03 +0900

    active hashの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
   validates :name, presence: true
   validates :url, format: { with: /\A[a-z0-9]+\z/i }
-  validates :category_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 8 }
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
