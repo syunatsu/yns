ce888369942ff73f1575b79c4a704ee4b0d409fa
syunatsu <myf.tyys@gmail.com>
2022-05-05 12:57:29 +0900

    active strageの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,7 @@ class Article < ApplicationRecord
   validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
 
   belongs_to :user
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
