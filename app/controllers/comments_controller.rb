85606cd26075b2e7390457c2ece0d2bdc34f7c42
syunatsu <myf.tyys@gmail.com>
2022-05-06 20:29:21 +0900

    authenticate_user!の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
   def create
     comment = Comment.create(comment_params)
     redirect_to "/articles/#{comment.article.id}"
